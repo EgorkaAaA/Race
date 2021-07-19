67b5d0acc26f77887d2c8faab19c3204dd9c2701
Егор Лебедев <m123ry132@gmail.com>
2021-07-19 15:53:28 +0700

    еще немного рефакторинга

--- Race/Program.cs
+++ Race/Program.cs
@@ -12,28 +12,40 @@ namespace Race
         static void Main(string[] args)
         {
             Console.WriteLine("Добро пожаловть в уличные гонки! " +
-                "\nДля того чтобы запустить гонку со случайными характеристиками машин нажмите 1" +
-                "\nДля того чтобы запустить гонку с настройкой своих характеристик нажмите 2");
+                "\nДля того чтобы запустить гонку со случайными характеристиками машин нажмите 1");
             ConsoleKey key = Console.ReadKey().Key;
+
             Console.Clear();
-            if(key == ConsoleKey.D1)
-            {
-                Truck truck = new Truck();
-                LightCar lightCar = new LightCar();
-                Motorcycle motorcycle = new Motorcycle();
 
-                Console.WriteLine(truck.ToString());
-                Console.WriteLine(lightCar.ToString());
-                Console.WriteLine(motorcycle.ToString());
 
-                Console.WriteLine("\n==========================НАЧАЛО ГОНКИ==========================\n");
+            while (true)
 
-                Console.WriteLine(RaceLogic.RaceLogic.result(truck));
-                Console.WriteLine(RaceLogic.RaceLogic.result(lightCar));
-                Console.WriteLine(RaceLogic.RaceLogic.result(motorcycle));
+                if(key == ConsoleKey.D1)
+                {
+                    Truck truck = new Truck();
+                    LightCar lightCar = new LightCar();
+                    Motorcycle motorcycle = new Motorcycle();
 
-                Console.ReadKey();
-            }
+                    Console.WriteLine(truck.ToString());
+                    Console.WriteLine(lightCar.ToString());
+                    Console.WriteLine(motorcycle.ToString());
+
+                    Console.WriteLine("\n==========================НАЧАЛО ГОНКИ==========================\n");
+
+                    Console.WriteLine(RaceLogic.RaceLogic.result(truck));
+                    Console.WriteLine(RaceLogic.RaceLogic.result(lightCar));
+                    Console.WriteLine(RaceLogic.RaceLogic.result(motorcycle));
+
+                    Console.WriteLine("\n==========================КОНЕЦ ГОНКИ==========================\n");
+
+                    Console.WriteLine("Вы хотите сделать еще один круг? 1 - да 2 - нет");
+
+                    ConsoleKey answer = Console.ReadKey().Key;
+
+                    if (answer == ConsoleKey.D2) break;
+
+                    Console.Clear();
+                }
         }
     }
 }
